1519c19d0836aff6fc91b4159c7d7f60c3e34d08
Debasis9090 <debasissahoochinku@gmail.com>
2019-04-18 03:31:45 -0700

    src/

---
+++ src/automation/SeleniumProject.java
@@ -0,0 +1,45 @@
+package automation;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumProject 
+{
+	private WebDriver driver;
+	public static void main(String[] args) 
+	{
+		SeleniumProject sp=new SeleniumProject();
+		sp.search();
+		sp.search1();
+	}
+	@Before
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\The Man Of Gamer\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	@Test
+	public void search()
+	{
+        driver.get("http:www.google.com");
+        driver.findElement(By.name("q")).sendKeys("https://www.sramanujan.com/");
+        driver.findElement(By.name("q")).submit();
+        driver.findElement(By.className("LC20lb")).click();
+	}
+	@Test
+	public void search1()
+	{
+        driver.get("http:www.google.com");
+        driver.findElement(By.name("q")).sendKeys("https://www.sramanujan.com/");
+        driver.findElement(By.name("q")).submit();
+        driver.findElement(By.className("LC20lb")).click();
+	}
+	@After
+	public void close()
+	{
+		driver.close();
+	}
+}
